c874411be86c38e71e4f9858646d0e1eb690c19d
0mbr3tta <ombrettaderiu@gmail.com>
2024-11-29 23:21:52 +0100

    quasi finite le parti obbligatorie

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,15 +1,22 @@
-
-const navbar = document.getElementById('navbar');
-const header = document.querySelector('header');
-
-
-window.addEventListener('scroll', () => {
-    
-    const headerHeight = header.offsetHeight;
-
-    if (window.scrollY > headerHeight) {
-        navbar.classList.add('sticky'); 
-    } else {
-        navbar.classList.remove('sticky'); 
+document.addEventListener('scroll', () => {
+const nav = document.querySelector('nav');
+    const button = document.querySelector('nav .Buttongetstarted');
+const mmm = document.querySelector('.MMM');
+const mmmBottom = mmm.getBoundingClientRect().bottom;
+if (mmmBottom <= 0) {
+        nav.
+style.backgroundColor = 'white';
+        button.
+style.backgroundColor = 'green';
+        button.
+style.color = 'white';
+    }
+else {nav.
+style.backgroundColor = '#FFC017';
+        button.
+      
+style.backgroundColor = 'black';
+        button.
+style.color = 'white';
     }
 });
